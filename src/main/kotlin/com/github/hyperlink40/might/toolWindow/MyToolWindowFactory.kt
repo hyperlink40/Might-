1ac42c830e852fdb50cd6baa6938c4ea4542bce5
GitHub Action <action@github.com>
2025-06-26 19:38:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/hyperlink40/might/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/hyperlink40/might/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.hyperlink40.might.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.hyperlink40.might.MyBundle
+import com.github.hyperlink40.might.services.MyProjectService
 import javax.swing.JButton
 
 
